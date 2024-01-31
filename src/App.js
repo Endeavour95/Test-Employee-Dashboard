4ab8439347985c80f02b69f150c0c5e56a6e13c8
Ashutosh Verulkar <ashutosh.verulkar@ayssoftwaresolution.com>
2024-01-31 19:26:53 +0000

    optimization in workig

--- src/App.js
+++ src/App.js
@@ -10,47 +10,53 @@ function DisplayEmployeeDetails(props) {
           Object.keys(props.employee).map((key, index) => {
             if (key !== "") {
               if (key === "empId") {
-                <tr key={index}>
-                  <td>
-                    <label>{labels[index]}</label>
-                  </td>
-                  <td>
-                    <input type="number" value={props.employee[key]} disabled />
-                  </td>
-                </tr>
-              }
-              if (key === "empDept") {
                 return (
                   <tr key={index}>
                     <td>
                       <label>{labels[index]}</label>
                     </td>
                     <td>
-                      <select id="myList" onChange={true} value={props.departments[props.employee[key]].deptId} disabled={props.editflag}>
-                        <option value="1" > IT </option>
-                        <option value="2" > HR </option>
-                        <option value="3" > Engineering </option>
+                      <input type="text" style={{ cursor: 'no-drop' }} value={props.employee[key]} disabled />
+                    </td>
+                  </tr>
+                )
+              } else if (key === "empDept") {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <label>{labels[index]}</label>
+                    </td>
+                    <td>
+                      <select id="myList" style={props.editFlag ? { cursor: 'no-drop' } : { cursor: 'pointer' }} value={props.employee.empDept} disabled={props.editFlag}>
+                        {
+                          props.departments.map((department) => {
+                            return (
+                              <option key={department.deptId} value={department.deptId} >{department.deptName}</option>
+                            )
+                          })
+                        }
                       </select>
                     </td>
                   </tr>
                 )
+              } else {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <label>{labels[index]}</label>
+                    </td>
+                    <td>
+                      <input type="text" value={props.employee[key]} style={props.editFlag ? { cursor: 'no-drop' } : { cursor: 'pointer' }} disabled={props.editFlag} />
+                    </td>
+                  </tr>
+                )
               }
-              return (
-                <tr key={index}>
-                  <td>
-                    <label>{labels[index]}</label>
-                  </td>
-                  <td>
-                    <input type="text" value={props.employee[key]} disabled={props.editflag} />
-                  </td>
-                </tr>
-              )
             }
           })
         }
         <tr>
           <td>
-            <button >Edit</button>
+            <button onClick={() => { props.seteditFlag(false) }} >Edit</button>
           </td>
           <td>
             <button >Delete</button>
@@ -61,40 +67,113 @@ function DisplayEmployeeDetails(props) {
   )
 }
 
-const List = (props) => {
-  let emplist = Object.keys(props.employees).map((empId) => {
-    return [empId, (props.employees[empId].empFName + " " + props.employees[empId].empLName)]
-  })
+function EmployeeRegistrationForm(props) {
+  const labels = ["Full Name : ", "Salary : ", "Designation : ", "Department : "]
+  const properties = ["empName", "empSalary", "empDesignation", "empDept"]
 
-  if (props.sortOrder) {
-    if (props.sortOrder === "DESC") {
-      let empArray = [...emplist]
-      empArray.sort((a, b) => b[1].localeCompare(a[1]));
-      emplist = empArray
-    }
-    if (props.sortOrder === "ASC") {
-      let empArray = [...emplist]
-      empArray.sort((a, b) => a[1].localeCompare(b[1]));
-      emplist = empArray
+  let employee = {
+    "empId": Number(props.employees[props.employees.length - 1].empId) + 1
+  }
+
+  const [newEmp, setnewEmp] = useState("")
+
+  function handleOnChange(e, property) {
+    if (property === "empDept") {
+      employee = { ...employee, [property]: e.target.value, }
+      console.log("handleOnChange", employee)
+      setnewEmp(employee)
+    } else {
+      employee = { ...employee, [property]: e.target.value, }
     }
   }
 
-  if (props.searchText !== "") {
-    let matchedEmployees = emplist.filter(([empId, empName]) =>
-      empName.toLowerCase().includes(props.searchText.trim().toLowerCase())
-    ).map(([empId]) => [empId, (props.employees[empId].empFName + " " + props.employees[empId].empLName)]);
+  return (
+    <table id="details">
+      <tbody>
+        {
+          properties.map((property, index) => {
+            if (property === "empDept") {
+              return (
+                <tr key={index}>
+                  <td>
+                    <label>{labels[index]}</label>
+                  </td>
+                  <td>
+                    <select id="myList" onChange={(e) => {
+                      handleOnChange(e, property)
+                      props.setSelectedDepartment(e.target.value)
+                    }}
+                      style={{ cursor: 'pointer' }} value={props.selectedDepartment} required>
+                      <option value="" disabled hidden>Select Department</option>
+                      {
+                        props.departments.map((department) => {
+                          return (
+                            <option key={department.deptId} value={department.deptId} >{department.deptName}</option>
+                          )
+                        })
+                      }
+                    </select>
+                  </td>
+                </tr>
+              )
+            } else {
+              return (
+                <tr key={index}>
+                  <td>
+                    <label>{labels[index]}</label>
+                  </td>
+                  <td>
+                    <input type="text" onChange={(e) => {
+                      handleOnChange(e, property)
+                    }} style={{ cursor: 'pointer' }} required />
+                  </td>
+                </tr>
+              )
+            }
+          })
+        }
+        <tr>
+          <td>
+            <button onClick={() => {
+              props.setEmployess([ ...props.employees, newEmp, ])
+            }}>Submit</button>
+          </td>
+          <td>
+            <button >Cancel</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
-    emplist = matchedEmployees
+
+const List = (props) => {
+  let dummyEmployees = [...props.employees]
+
+  if (props.sortOrder) {
+    if (props.sortOrder === "DESC") {
+      dummyEmployees.sort((a, b) => b.empName.localeCompare(a.empName));
+    }
+    if (props.sortOrder === "ASC") {
+      dummyEmployees.sort((a, b) => a.empName.localeCompare(b.empName));
+    }
+  }
+
+  if (props.searchText) {
+    dummyEmployees = props.employees.filter(employee =>
+      employee.empName.toLowerCase().includes(props.searchText.toLowerCase())
+    );
   }
 
   return (
     <tbody>
       {
-        emplist.map((empId, index) => {
+        dummyEmployees.map((employee, index) => {
           return (
-            <tr key={index}><td><input type="button" style={props.selectedEmployeeId === emplist[index][0] ? { backgroundColor: "yellowgreen" } : { backgroundColor: "#4caf50" }}
-              value={emplist[index][1]}
-              onClick={() => { props.setSelectedEmployeeId(emplist[index][0]) }} ></input></td></tr>
+            <tr key={index}><td><input type="button" style={props.selectedEmployee.empId === employee.empId ? { backgroundColor: "yellowgreen" } : { backgroundColor: "#4caf50" }}
+              value={employee.empName}
+              onClick={() => { props.setSelectedEmployee(employee) }} ></input></td></tr>
           )
         })
       }
@@ -107,9 +186,9 @@ const NavBar = (props) => {
   return (
     <nav>
       {
-        Object.keys(props.names).map((name) => {
+        props.names.map((name, index) => {
           return (
-            <input key={name} type="button" defaultValue={name} onClick={props.names[name]} />
+            <input key={name} id={index + 1} type="button" defaultValue={name} onClick={(e) => props.setclickedButton(e.target.id)} />
           )
         })
       }
@@ -134,85 +213,76 @@ const Sort = (props) => {
   )
 }
 
-function App() {
-  const departments = {
-    '1': {
+const EmployeesDashboard = () => {
+  const departments = [
+    {
       deptId: 1,
       deptName: "IT"
     },
-    '2': {
+    {
       deptId: 2,
       deptName: "HR"
     },
-    '3': {
+    {
       deptId: 3,
       deptName: "Engineering"
     }
-  }
+  ]
 
-  let employees = {
-    '1': {
+  let employees1 = [
+    {
       empId: 1,
-      empFName: "Ashutosh",
-      empLName: "Verulkar",
+      empName: "Ashutosh Verulkar",
       empSalary: "20000",
       empDesignation: "Developer",
-      empDept: departments[1]?.deptId
+      empDept: departments[2].deptId
     },
-    '2': {
+    {
       empId: 2,
-      empFName: "Nikhilesh",
-      empLName: "Mane",
+      empName: "Nikhilesh Mane",
       empSalary: "30000",
       empDesignation: "Team Lead",
-      empDept: departments[2]?.deptId
+      empDept: departments[1].deptId
     },
-    '3': {
+    {
       empId: 3,
-      empFName: "Harshal",
-      empLName: "Dhokane",
+      empName: "Harshal Dhokane",
       empSalary: "37000",
       empDesignation: "Developer",
-      empDept: departments[1]?.deptId
+      empDept: departments[2].deptId
     },
-    '4': {
+    {
       empId: 4,
-      empFName: "Sanket",
-      empLName: "Gupta",
+      empName: "Sanket Gupta",
       empSalary: "45000",
       empDesignation: "Data Engineer",
-      empDept: departments[3]?.deptId
+      empDept: departments[0]?.deptId
     }
-  }
+  ]
 
-  const [selectedEmployeeId, setSelectedEmployeeId] = useState('')
+  const [employees, setEmployess] = useState(employees1)
 
-  // const employessNamelist = clonedEmployessNames(employees)
+  const [selectedEmployee, setSelectedEmployee] = useState('')
 
-  const [editflag, setEditflag] = useState(selectedEmployeeId ? false : true)
+  const [selectedDepartment, setSelectedDepartment] = useState('')
+
+  const [editFlag, seteditFlag] = useState(selectedEmployee ? false : true)
+  // const [editflag, setEditflag] = useState(false)
+
+  const [clickedButton, setclickedButton] = useState('')
 
   const [searchText, setSearchText] = useState('')
 
   const [sortOrder, setSortOrder] = useState("")
 
-  // function clonedEmployessNames(employees) {
-  //   let emplist = {}
-  //   Object.keys(employees).map((empId) => {
-  //     emplist[empId] = employees[empId].empFName + " " + employees[empId].empLName
-  //   })
-  //   return emplist
-  // }
-
-
   return (
     <>
       <div id="navdiv">
-        <NavBar names={{ "Home": App, "Add Employee": App }} />
+        <NavBar names={["Home", "Add Employee"]} setclickedButton={setclickedButton} />
       </div>
       <div id="downdiv">
         <div id="left">
           <div id='toolbar'>
-            {/* <Search setClonedEmployessNamelist={setClonedEmployessNamelist} employessNamelist={employessNamelist} /> */}
             <Search setSearchText={setSearchText} />
             <Sort setSortOrder={setSortOrder} sortOrder={sortOrder} />
           </div>
@@ -223,8 +293,7 @@ function App() {
                   <th>Employees List</th>
                 </tr>
               </thead>
-              {/* <List clonedEmployess={clonedEmployess} setEmpId={setEmpId} selectedEmployeeId={empId} /> */}
-              <List employees={employees} setSelectedEmployeeId={setSelectedEmployeeId} selectedEmployeeId={selectedEmployeeId} searchText={searchText} sortOrder={sortOrder} />
+              <List employees={employees} setSelectedEmployee={setSelectedEmployee} selectedEmployee={selectedEmployee} searchText={searchText} sortOrder={sortOrder} />
             </table>
           </div>
         </div>
@@ -233,11 +302,23 @@ function App() {
             <h3>Employee Details</h3>
           </div>
           <div id="view">{
-            selectedEmployeeId ? <DisplayEmployeeDetails employee={employees[selectedEmployeeId]} departments={departments} editflag={editflag} /> : <p>Please select an employee to view details.</p>
+            selectedEmployee ? <DisplayEmployeeDetails employee={selectedEmployee} departments={departments} editFlag={editFlag} seteditFlag={seteditFlag} /> : <p>Please select an employee to view details.</p>
           }
           </div>
+
+          {/* <div>
+            <EmployeeRegistrationForm employees={employees} setEmployess={setEmployess} departments={departments} selectedDepartment={selectedDepartment} setSelectedDepartment={setSelectedDepartment} />
+          </div> */}
         </div>
       </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <EmployeesDashboard />
     </>
   )
 }
